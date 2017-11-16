cf8341121be7ae588125191e43720a14a5c9731f
zcbmlijygrdwa <zcbmlijygrdwa@ymail.com>
2017-11-16 11:37:00 -0800

    add OpenMP example

---
+++ OpenMP/omp.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <omp.h>
+
+int main()
+{
+#pragma omp parallel for
+    for (char i = 'a'; i <= 'z'; i++)
+        std::cout << i << std::endl;
+
+    return 0;
+}
+
